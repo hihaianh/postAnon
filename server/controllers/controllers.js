feefc1cf3b2a9067cbd6d7f8d599d2bbfd6dbad0
Hai Anh Nguyen <haianhnguyen@Hais-MacBook-Air.local>
2022-03-23 12:13:35 +0000

    finding story by id

--- server/controllers/controllers.js
+++ server/controllers/controllers.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const router = express.Router();
-
-
 const Story = require('../models/models.js')
+
+
+router.get('/:id', async (req, res) => {
+    try {
+        const story = await Story.findStoryById(req.params.id)
+        res.json(story)
+    } catch(err) {
+        res.status(404).send("Not found")
+    }
+})
+
