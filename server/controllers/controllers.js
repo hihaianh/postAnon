992a11c9bbd4d18b4761df5f92b33c10f2f30226
Hai Anh Nguyen <haianhnguyen@Hais-MacBook-Air.local>
2022-03-23 12:25:51 +0000

    added post

--- server/controllers/controllers.js
+++ server/controllers/controllers.js
@@ -8,7 +8,15 @@ router.get('/:id', async (req, res) => {
         const story = await Story.findStoryById(req.params.id)
         res.json(story)
     } catch(err) {
-        res.status(404).send("Not found")
+        res.status(404).json({err})
     }
 })
 
+router.post('/', async (req, res) => {
+    try {
+        const story = await Story.create(req.body)
+        res.json(story)
+    } catch(err) {
+        res.status(404).json({err})
+    }
+})
