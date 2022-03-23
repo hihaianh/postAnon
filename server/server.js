af74cafff1e5d069ad5d96c0303032b1ea5f526c
idrisimo <idrissilva@hotmail.com>
2022-03-23 12:15:54 +0000

    server container starting now

--- server/server.js
+++ server/server.js
@@ -16,3 +16,15 @@ server.get('/', (req, res) => {
 })
 
 
+module.exports = server
+
+// const express = require('express');
+// const cors = require('cors');
+
+// const server = express();
+// server.use(cors());
+// server.use(express.json());
+
+// server.get('/', (req, res) => res.send('Welcome to the library'))
+
+// module.exports = server
