21cb913ea883b2758589f9aeefebd9fd4c3b61ed
Hai Anh Nguyen <haianhnguyen@Hais-MacBook-Air.local>
2022-03-23 14:48:51 +0000

    get all method and various changes made

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const path = require('path');
-app.use(cors());
+
+app.use(cors('*'));
 app.use(express.json());
 
 // for post requests- support parsing of json type post data
