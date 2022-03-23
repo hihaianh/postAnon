8a7ff441d33636a35b91235ecb32acaa20a3f2e2
Hai Anh Nguyen <haianhnguyen@Hais-MacBook-Air.local>
2022-03-23 15:56:14 +0000

    rid of body-parser

--- server/server.js
+++ server/server.js
@@ -5,15 +5,11 @@ const app = express();
 app.use(cors('*'));
 app.use(express.json());
 
-// for post requests- support parsing of json type post data
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 const storyRoutes = require('./controllers/controllers')
 app.use('/', storyRoutes)
 
-//sending index.html to homepage
+
 app.get('/', (req, res) => {
     res.send('hello world')
 })
